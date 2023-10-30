7ac2f80034bde713c594bff9c42a46bad2e8e8b2
leath-dub <fierceinbattle@gmail.com>
2023-10-30 15:22:56 +0000

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "My neovim config";
+
+  inputs = {
+    nixpkgs.url = "flake:nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "bspwm-conf";
+        src = ./.;
+
+        installPhase = ''
+          mkdir -p $out
+          cp -r $src $out
+        '';
+
+        meta = {
+          description = "Derivation storing my bspwm configuration";
+        };
+      };
+      dest = "./.config/bspwm";
+    });
+}
+
